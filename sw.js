ca16274bd828f128a048cda051609955303d714c
Style <danielrvix@gmail.com>
2026-08-22 20:25:59 -0400

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-self.addEventListener('fetch', (event) => {
-  // Service worker básico obligatorio para PWA
-  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
-});
